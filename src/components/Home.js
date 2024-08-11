1023e4d3301d0e6e974e81ca9833fc109b89280b
duttmistry <dutt.cybercom@gmail.com>
2024-08-11 17:05:27 +0530

    Changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,10 @@
 import { useMsal } from "@azure/msal-react";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const { accounts, instance } = useMsal();
+  const navigate = useNavigate();
   console.log("accounts: ", accounts);
 
   const logout = () => {
@@ -14,6 +16,7 @@ const Home = () => {
     <div>
       <h1>Home Page </h1>
       <p>Welcome {accounts[0]?.name}</p>
+      <button onClick={() => navigate("/profile")}>Profile</button>
       <button onClick={logout}>Signout</button>
     </div>
   );
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,9 +2,11 @@ import React from "react";
 import { useMsal } from "@azure/msal-react";
 import { callMsGraph } from "../graph"; // Ensure this is correctly implemented
 import { loginRequest } from "../authConfig";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
   const { instance, accounts } = useMsal();
+  const navigate = useNavigate();
   const [profileData, setProfileData] = React.useState(null);
 
   React.useEffect(() => {
@@ -21,14 +23,21 @@ const Profile = () => {
         });
     }
   }, [accounts, instance]);
-
+  const logout = () => {
+    instance.logoutRedirect({
+      postLogoutRedirectUri: "/",
+    });
+  };
   return (
     <div>
       <h1>Profile Page</h1>
+      {console.log("profileData: ", profileData)}
       {profileData ? (
         <div>
           <h2>Welcome, {profileData.displayName}</h2>
-          <p>Email: {profileData.mail}</p>
+          <p>Email: {profileData.userPrincipalName}</p>
+          <button onClick={() => navigate("/")}>back</button>
+          <button onClick={logout}>Logout</button>
         </div>
       ) : (
         <p>Loading...</p>
